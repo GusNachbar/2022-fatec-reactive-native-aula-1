5dcc6025bc092954363f9d586017e852cc735d15
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-18 16:35:31 -0300

    Exercicio de exibicao de temp min, temp max e description resolvido com for comum

--- aula.js
+++ aula.js
@@ -3,7 +3,7 @@ const axios = require ('axios')
 
 const { PROTOCOL, BASE_URL, APPID, Q, CNT, UNITS, LANGUAGE } = process.env
 
-const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&cnt=${CNT}&UNITS=${UNITS}&lang=${LANGUAGE}`
+const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&cnt=${CNT}&units=${UNITS}&lang=${LANGUAGE}`
 
 axios.get(url)
 .then(res => {
@@ -17,5 +17,19 @@ axios.get(url)
 .then(res => {
     console.log(res.cnt)
     return res
+})
+.then(res => {
+    return res['list']
+})
+.then(res => {
+    for (let i = 0; i < res.length; i++){
+        console.log ("Temp min: " + res[i].main.temp_min)
+        console.log ("Temp max: " + res[i].main.temp_max)
+        console.log("Descrição: " + res[i].weather[0].description)
+    }
+    //para cada previsão, exibir:
+    //temperatura máxima
+    //temperatura mínima
+    //descrição
 })
 
