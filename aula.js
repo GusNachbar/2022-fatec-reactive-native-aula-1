5222937b035b90e63adc324349db1261d17bd18a
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-18 16:14:32 -0300

    Consumo de WS e encadeamento de promises. Exercicio para exibir temp min, max e descricao proposto

--- aula.js
+++ aula.js
@@ -1,2 +1,21 @@
 require('dotenv').config()
-console.log(process.env.API_KEY)
+const axios = require ('axios')
+
+const { PROTOCOL, BASE_URL, APPID, Q, CNT, UNITS, LANGUAGE } = process.env
+
+const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&cnt=${CNT}&UNITS=${UNITS}&lang=${LANGUAGE}`
+
+axios.get(url)
+.then(res => {
+    //console.log(res)
+    return res.data
+})
+.then(res => {
+    console.log(res)
+    return res
+})
+.then(res => {
+    console.log(res.cnt)
+    return res
+})
+
