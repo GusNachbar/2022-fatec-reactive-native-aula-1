ff32204f7ec2789c976044645184d40cb9b60d36
Gustavo Nachbar <gnachbar.breve@gmail.com>
2022-03-18 21:38:43 -0300

    Correção no método fatorial

--- aulaCont.js
+++ aulaCont.js
@@ -11,13 +11,11 @@ resultado.then(r => {console.log(r);})
 
 function fatorial(n) {
     if(n < 0) return Promise.reject("Valor não pode ser negativo")
-    if(n === 0 || n === 1) {
-        const res = 1
-        return Promise.resolve(res)
-    }
-    
-    
+    let res = 1
+    for(let i = 2; i < n; i++) res *= i
+    return Promise.resolve(res)
 }
+
 
 function chamadaComThenCatch(){
     fatorial(10)
@@ -30,8 +28,22 @@ function chamadaComThenCatch(){
 }
 
 async function chamadaComAsyncAwait(){
+
     const f1 = await fatorial(10)
     console.log(f1)
     const f2 = await fatorial(-10)
     console.log(f2)
 }
+
+async function chamadaComTryCatch(){
+    try{
+        const f1 = await fatorial(10)
+        console.log(f1)
+        const f2 = await fatorial(-10)
+        console.log(f2)
+    }catch(e){
+        console.log(e);
+    }
+}
+
+console.log(fatorial(6))
